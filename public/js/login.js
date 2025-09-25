1e01f43906f35938768e255ae114f495ad30949a
Ankitpal2005 <ap7023278@gmail.com>
2025-09-25 11:28:28 +0530

    login issue

--- public/js/login.js
+++ public/js/login.js
@@ -7,7 +7,7 @@ async function login() {
     return;
   }
   try {
-    const response = await fetch("http://localhost:3000/api/auth/login", {
+    const response = await fetch("/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
