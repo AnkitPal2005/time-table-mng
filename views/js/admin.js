44949a1bcb06f316bcf60500c1749896827d0777
Ankitpal2005 <ap7023278@gmail.com>
2025-09-25 11:19:35 +0530

    Third Commit

--- views/js/admin.js
+++ views/js/admin.js
@@ -1,342 +1,3 @@
-// function openForm() {
-//   document.getElementById("formModal").style.display = "flex";
-// }
-
-// function closeForm() {
-//   document.getElementById("formModal").style.display = "none";
-// }
-
-// async function saveLecture() {
-//   const teacher = document.getElementById("teacherSelect").value;
-//   const subject = document.getElementById("subject").value.trim();
-//   const room = document.getElementById("room").value.trim();
-//   const day = document.getElementById("day").value;
-//   const period = parseInt(document.getElementById("period").value);
-//   const startTime = document.getElementById("startTime").value;
-//   const endTime = document.getElementById("endTime").value;
-
-//   if (
-//     !teacher ||
-//     !subject ||
-//     !room ||
-//     !day ||
-//     !period ||
-//     !startTime ||
-//     !endTime
-//   ) {
-//     alert("Please fill all fields");
-//     return;
-//   }
-
-//   const newLecture = {
-//     teacher,
-//     subject,
-//     room,
-//     day,
-//     period,
-//     startTime,
-//     endTime,
-//   };
-//   // console.log(newLecture.teacher.value);
-//   // const lectures = JSON.parse(localStorage.getItem("lectures")) || [];
-//   // lectures.push(newLecture);
-//   // localStorage.setItem("lectures", JSON.stringify(lectures));
-//   try {
-//     const response = await fetch("http://localhost:3000/api/auth/lectures", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify(newLecture),
-//     });
-//     const data = await response.json();
-//     if (response.ok) {
-//       alert("Lecture added Successfuly");
-//       closeForm();
-//       renderTable();
-//     } else {
-//       alert("Error adding lecture" + data.message);
-//     }
-//   } catch (err) {
-//     console.error("Fetch Error", err);
-//     alert("Please Try again later");
-//   }
-// }
-// document.getElementById("logoutBtn").addEventListener("click", async () => {
-//   try {
-//     const res = await fetch("http://localhost:3000/api/auth/logout", {
-//       method: "POST",
-//       credentials: "include",
-//     });
-
-//     const data = await res.json();
-//     alert(data.message);
-
-//     if (res.status === 200) {
-//       window.location.href = "/login.html";
-//     }
-//   } catch (err) {
-//     console.error("Logout failed:", err);
-//     alert("Logout failed. Try again.");
-//   }
-// });
-
-// async function renderTable() {
-//   const tbody = document.getElementById("timetable-body");
-//   tbody.innerHTML = "";
-
-//   const selectedTeacher = document.getElementById("teacherFilter").value;
-//   // const lectures = JSON.parse(localStorage.getItem("lectures")) || [];
-
-//   console.log(selectedTeacher);
-//   const dayList = [
-//     "monday",
-//     "tuesday",
-//     "wednesday",
-//     "thursday",
-//     "friday",
-//     "saturday",
-//   ];
-//   let lectures = [];
-
-//   try {
-//     const response = await fetch(
-//       `http://localhost:3000/api/auth/showLectures/${selectedTeacher}`
-//     );
-//     const data = await response.json();
-//     if (response.ok) {
-//       lectures = data.lectures;
-//       console.log(lectures,"hare krishna");
-      
-//     } else {
-//       alert("Error fetching lectures" + data.message);
-//       return;
-//     }
-//   } catch (error) {
-//     console.error("Fetch Error", error);
-//     alert("network error");
-//     return;
-//   }
-//   dayList.forEach((day) => {
-//     const row = document.createElement("tr");
-//     const dayCell = document.createElement("td");
-//     dayCell.textContent = day;
-//     row.appendChild(dayCell);
-
-//     for (let i = 1; i <= 8; i++) {
-//       const cell = document.createElement("td");
-//       console.log(day,selectedTeacher,i);
-//       const lecture = lectures.find(
-//         (l) => l.teacher == selectedTeacher && l.day == day && l.period == i
-//       );
-//       console.log(lecture,"ooooooooooooooooooo");
-//       cell.innerHTML = lecture
-//         ? `<strong>${lecture.subject}</strong><br>(${lecture.room})<br><small>${lecture.startTime} - ${lecture.endTime}</small>`
-//         : "-";
-//       row.appendChild(cell);
-//     }
-
-//     tbody.appendChild(row);
-//   });
-// }
-
-// async function loadTeachers() {
-//   // const users = JSON.parse(localStorage.getItem("users")) || [];
-
-//   const filterDropdown = document.getElementById("teacherFilter");
-//   const formDropdown = document.getElementById("teacherSelect");
-
-//   filterDropdown.innerHTML = "";
-//   formDropdown.innerHTML = "";
-//   try {
-//     const response = await fetch("http://localhost:3000/api/auth/teachers", {
-//       credentials: "include",
-//     });
-//     const data = await response.json();
-//     console.log("response", data);
-//     const teachers = data.teachers;
-//     console.log(teachers);
-//     teachers.forEach((teacher) => {
-//       const option1 = document.createElement("option");
-//       option1.value = teacher.username;
-//       option1.textContent = teacher.username;
-
-//       const option2 = option1.cloneNode(true);
-
-//       filterDropdown.appendChild(option1);
-//       formDropdown.appendChild(option2);
-//     });
-//     renderTable();
-//   } catch (error) {
-//     console.log("error", error);
-//   }
-// }
-// async function loadleaverequests() {
-//   const leaveListDiv = document.getElementById("leave-list");
-//   leaveListDiv.innerHTML = "Loading Leave Request......";
-//   try {
-//     const res = await fetch("http://localhost:3000/api/auth/leave", {
-//       credentials: "include",
-//     });
-//     const leaves = await res.json();
-//     if (!Array.isArray(leaves) || !leaves.length) {
-//       leaveListDiv.innerHTML = "No Leave Request.";
-//       return;
-//     }
-//     leaveListDiv.innerHTML = "";
-//     leaves.forEach((leave, index) => {
-//       console.log(leave.day)
-//       const leaveDiv = document.createElement("div");
-//       leaveDiv.classList.add("leave-request");
-//       leaveDiv.style.border = "1px solid #ccc";
-//       leaveDiv.style.padding = "10px";
-//       leaveDiv.style.marginBottom = "10px";
-//       leaveDiv.innerHTML = `
-//         <strong>${leave.username}</strong> has requested leave for <strong>${
-//         leave.day
-//       }</strong>
-//         <em>Reason:</em> ${leave.reason} <br>
-//         <em>Status:</em> ${leave.status || "pending"} <br><br>
-//         <button onclick="handleApprove('${leave.username}', '${
-//         leave.day
-//       }', 'approved')">Approve</button>
-//         <button onclick="handleReject('${leave.username}', '${
-//         leave.day
-//       }', 'rejected')">Reject</button>
-//         <button onclick="handleAdjust('${leave.username}', '${
-//         leave.day
-//       }')">Adjust</button>
-//       `;
-//       leaveListDiv.appendChild(leaveDiv);
-//     });
-//   } catch (error) {
-//     console.error("Error fetching leave requests", error);
-//     leaveListDiv.innerHTML = "Error loading leave requests.";
-//   }
-// }
-
-// async function handleAdjust(username, day) {
-//   console.log(day)
-//   try {
-//     const res = await fetch("http://localhost:3000/api/auth/getlecturesleave", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       credentials: "include",
-//       body: JSON.stringify({ username, day }),
-//     });
-
-//     const data = await res.json();
-//     console.log(data)
-//     const result = data.lectures
-//     const availableTeachers=data.availableTeachers;
-//     if (res.ok) {
-//       const container = document.getElementById("adjustLectureList");
-//       container.innerHTML = "";
-
-//       if (!result.length) {
-//         container.innerHTML = `<p>No lectures found for ${username} on ${day}</p>`;
-//       } else {
-//         const ul = document.createElement("ul");
-//         result.forEach((lecture) => {
-//           const li = document.createElement("li");
-//           let options=availableTeachers.map(
-//             t=>`<option value=${t.username}">${t.username} (Load: ${t.load})</option>`
-//           ).join("");
-//           li.innerHTML = `
-//             <strong>Subject:</strong> ${lecture.subject} <br>
-//             <strong>Day:</strong> ${lecture.day} <br>
-//             <strong>Room:</strong> ${lecture.room} <br>
-//             <strong>Period:</strong> ${lecture.period} <br>
-//             <strong>Time:</strong> ${lecture.startTime} - ${lecture.endTime}
-//             <select>
-//             ${options}
-//             </select>
-//           `;
-//           ul.appendChild(li);
-//         });
-//         container.appendChild(ul);
-//       }
-
-//       document.getElementById("teacherLectureModal").style.display = "block";
-//     } else {
-//       alert(result.message || "Failed to adjust leave request.");
-//     }
-//   } catch (error) {
-//     console.error("Error adjusting leave request:", error);
-//     alert("Failed to adjust leave request. Please try again.");
-//   }
-// }
-
-// function closeTeacherLectures() {
-//   document.getElementById("teacherLectureModal").style.display = "none";
-// }
-
-// async function handleReject(username, day) {
-//   try {
-//     const res = await fetch("http://localhost:3000/api/auth/reject", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       credentials: "include",
-//       body: JSON.stringify({ username, day}),
-//     });
-//     const result = await res.json();
-//     if (res.ok) {
-//       alert("Leave request rejected successfully!");
-//       loadleaverequests();
-//     } else {
-//       alert(result.message || "Failed to rejected leave request.");
-//     }
-//   } catch (error) {
-//     console.error("Error adjusting leave request:", error);
-//     alert("Failed to adjust leave request. Please try again.");
-//   }
-// }
-
-//   async function handleApprove(username, day,status) {
-//     try {
-//       const res = await fetch("http://localhost:3000/api/auth/approveleave", {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         credentials: "include",
-//         body: JSON.stringify({ username, day }),
-//       });
-//       const result = await res.json();
-//       if (res.ok) {
-//         alert(`Leave ${status} successfully!`);
-//         loadleaverequests();
-//       } else {
-//         alert(result.message || `Failed to ${status} leave request.`);
-//       }
-//     } catch (error) {
-//       console.error(`Error ${status} leave request:`, error);
-//       alert(`Failed to ${status} leave request. Please try again.`);
-//     }
-//   };
-
-// document
-//   .getElementById("teacherFilter")
-//   .addEventListener("change", renderTable);
-
-// window.onload = function () {
-//   const dt = document.getElementById("date-time");
-//   if (dt) dt.innerText = new Date().toLocaleString();
-//   loadTeachers();
-//   loadleaverequests();
-// };
-
-// document.getElementById("show-leave-btn").addEventListener("click", () => {
-//   const leaveSection = document.getElementById("leave-requests-section");
-//   if (leaveSection.style.display === "none" || !leaveSection.style.display) {
-//     leaveSection.style.display = "block";
-//     loadleaverequests();
-//   } else {
-//     leaveSection.style.display = "none";
-//   }
-// });
-
-
 function openForm() {
   document.getElementById("formModal").style.display = "flex";
 }
@@ -381,7 +42,7 @@ async function saveLecture() {
   // lectures.push(newLecture);
   // localStorage.setItem("lectures", JSON.stringify(lectures));
   try {
-    const response = await fetch("http://localhost:3000/api/auth/lectures", {
+    const response = await fetch("/api/auth/lectures", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newLecture),
@@ -401,7 +62,7 @@ async function saveLecture() {
 }
 document.getElementById("logoutBtn").addEventListener("click", async () => {
   try {
-    const res = await fetch("http://localhost:3000/api/auth/logout", {
+    const res = await fetch("/api/auth/logout", {
       method: "POST",
       credentials: "include",
     });
@@ -438,7 +99,7 @@ async function renderTable() {
 
   try {
     const response = await fetch(
-      `http://localhost:3000/api/auth/showLectures/${selectedTeacher}`
+      `/api/auth/showLectures/${selectedTeacher}`
     );
     const data = await response.json();
     if (response.ok) {
@@ -485,7 +146,7 @@ async function loadTeachers() {
   filterDropdown.innerHTML = "";
   formDropdown.innerHTML = "";
   try {
-    const response = await fetch("http://localhost:3000/api/auth/teachers", {
+    const response = await fetch("/api/auth/teachers", {
       credentials: "include",
     });
     const data = await response.json();
@@ -511,7 +172,7 @@ async function loadleaverequests() {
   const leaveListDiv = document.getElementById("leave-list");
   leaveListDiv.innerHTML = "Loading Leave Request......";
   try {
-    const res = await fetch("http://localhost:3000/api/auth/leave", {
+    const res = await fetch("/api/auth/leave", {
       credentials: "include",
     });
     const leaves = await res.json();
@@ -553,7 +214,7 @@ async function loadleaverequests() {
 
 async function handleAdjust(username, day) {
   try {
-    const res = await fetch("http://localhost:3000/api/auth/getlecturesleave", {
+    const res = await fetch("/api/auth/getlecturesleave", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -610,7 +271,7 @@ function closeTeacherLectures() {
 
 async function handleReject(username, day) {
   try {
-    const res = await fetch("http://localhost:3000/api/auth/reject", {
+    const res = await fetch("/api/auth/reject", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -633,7 +294,7 @@ async function handleReject(username, day) {
 
 async function handleApprove(username, day, status) {
   try {
-    const res = await fetch("http://localhost:3000/api/auth/approveleave", {
+    const res = await fetch("/api/auth/approveleave", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       credentials: "include",
@@ -675,7 +336,7 @@ document.getElementById("show-leave-btn").addEventListener("click", () => {
 async function AssignTeacher(username, day, period, startTime, subject, room, endTime){
     console.log("Clicked AssignTeacher for:", username, day, period, startTime);
 try{
-    const res = await fetch("http://localhost:3000/api/auth/assignteacher", {
+    const res = await fetch("/api/auth/assignteacher", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       credentials: "include",
@@ -734,7 +395,7 @@ try{
 }
 async function assignLectureToTeacher(teacher,day,period,startTime,subject,room,endTime) {
   try {
-    const res = await fetch("http://localhost:3000/api/auth/assignLecture", {
+    const res = await fetch("/api/auth/assignLecture", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       credentials: "include",
--- views/js/teacher.js
+++ views/js/teacher.js
@@ -1,165 +1,3 @@
-// // const { post } = require("../../routes/auth");
-// console.log("hello ji ankit kya hal hai ");
-// const params = new URLSearchParams(window.location.search);
-// console.log(window.location.search);
-// document
-//   .querySelector("aside.sidebar button:nth-Child(1)")
-//   .addEventListener("click", () => {
-//     const user = JSON.parse(localStorage.getItem("user"));
-//     window.location.href = "/views/dashboard.html?username=" + user.user;
-//   });
-// document
-//   .querySelector("aside.sidebar button:nth-Child(2)")
-//   .addEventListener("click", () => {
-//     const user = JSON.parse(localStorage.getItem("user"));
-//     window.location.href = "/views/teacher.html?username=" + user.user;
-//   });
-
-// const username = params.get("username");
-// console.log(username); // Ankit pal
-
-// document.getElementById("teacher-name").innerText = `${
-//   username || "Teacher"
-// } (Teacher)`;
-// const now = new Date();
-// document.getElementById("current-date").innerText = `${now.getDate()}/${
-//   now.getMonth() + 1
-// }/${now.getFullYear()}`;
-// document.getElementById("current-time").innerText = now.toLocaleTimeString();
-// document.getElementById("logoutBtn").addEventListener("click", async () => {
-//   try {
-//     const res = await fetch("http://localhost:3000/api/auth/logout", {
-//       method: "POST",
-//       credentials: "include",
-//     });
-
-//     const data = await res.json();
-//     alert(data.message);
-
-//     if (res.status === 200) {
-//       window.location.href = "/login.html";
-//     }
-//   } catch (err) {
-//     console.error("Logout failed:", err);
-//     alert("Logout failed. Try again.");
-//   }
-// });
-// async function fetcTeachers(req, res) {
-//   try {
-//     const response = await fetch(
-//       `http://localhost:3000/api/auth/getLectureByTeacher/teacher/${username}`,
-//       {
-//         credentials: "include",
-//       }
-//     );
-//     const data = await response.json();
-//     console.log(data);
-
-//     return data;
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-// async function renderTeacherTimetable() {
-//   const lectures = await fetcTeachers();
-//   console.log(lectures, "kya hakl hai");
-//   const tbody = document.getElementById("teacher-timetable");
-//   tbody.innerHTML = "";
-
-//   const days = [
-//     "Monday",
-//     "Tuesday",
-//     "Wednesday",
-//     "Thursday",
-//     "Friday",
-//     "Saturday",
-//   ];
-//   console.log(days);
-//   days.forEach((day) => {
-//     const row = document.createElement("tr");
-//     const dayCell = document.createElement("td");
-//     dayCell.textContent = day;
-//     row.appendChild(dayCell);
-
-//     for (let i = 1; i <= 8; i++) {
-//       const cell = document.createElement("td");
-//       const lecture = lectures?.find(
-//         (l) => l.teacher === username && l.day === day && l.period === i
-//       );
-
-//       cell.innerHTML = lecture
-//         ? `<strong>${lecture.subject}</strong><br>(${lecture.room})<br><small>${lecture.startTime} - ${lecture.endTime}</small>`
-//         : "-";
-//       row.appendChild(cell);
-//     }
-
-//     tbody.appendChild(row);
-//   });
-// }
-// const approveLeaveBtn = Array.from(document.querySelectorAll("button")).find(
-//   (btn) => btn.textContent.trim() === "Request-Leave"
-// );
-
-// if (approveLeaveBtn) {
-//   approveLeaveBtn.addEventListener("click", () => {
-//     const leaveFormSection = document.getElementById("leave-form-section");
-//     leaveFormSection.style.display =
-//       leaveFormSection.style.display === "none" ? "block" : "none";
-//   });
-// }
-
-// // Handle Leave Form Submission
-// const leaveForm = document.getElementById("leave-Form");
-// if (leaveForm) {
-//   leaveForm.addEventListener("submit", async (e) => {
-//     e.preventDefault();
-
-//     // const formData = new FormData(leaveForm);
-//     const day = document.querySelector(".day-leave-select").value;
-//     const reason = document.getElementById("reason").value;
-//     if (!day || !reason) {
-//       alert("Please fill all fields");
-//       if (!day) document.querySelector
-//       (".day-leave-select").focus();
-//       if (!reason) document.getElementById("reason").focus();
-//       return;
-//     }
-//     const leaveData = {
-//       username: username || "unknown",
-//       day,
-//       reason,
-//     };
-
-//     try {
-//       const res = await fetch("http://localhost:3000/api/auth/request", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(leaveData),
-//       });
-
-//       const result = await res.json();
-//       if (res.ok) {
-//         document.getElementById("leave-success").innerText =
-//           "Leave request submitted successfully!";
-//         leaveForm.reset();
-//       } else {
-//         alert(result.message || "Failed to submit leave.");
-//       }
-//     } catch (error) {
-//       console.error("Error submitting leave:", error);
-//     }
-//   });
-// }
-// document.getElementById("cancel").addEventListener("click", (e) => {
-//   e.preventDefault();
-//   document.getElementById("leave-form-section").style.display = "none";
-//   document.getElementById(leave-success).style.innerHTML="";
-// });
-
-// window.onload = renderTeacherTimetable;
-// const { post } = require("../../routes/auth");
 let username;
 document
   .querySelector("aside.sidebar button:nth-Child(1)")
@@ -177,7 +15,7 @@ document
   });
   async function getsession() {
     try {
-      const res = await fetch("http://localhost:3000/getsession", {
+      const res = await fetch("/api/getsession", {
         method: "GET",
         credentials: "include",
       });
@@ -211,7 +49,7 @@ document
 
 document.getElementById("logoutBtn").addEventListener("click", async () => {
   try {
-    const res = await fetch("http://localhost:3000/api/auth/logout", {
+    const res = await fetch("/api/auth/logout", {
       method: "POST",
       credentials: "include",
     });
@@ -232,7 +70,7 @@ document.getElementById("logoutBtn").addEventListener("click", async () => {
 async function fetcTeachers() {
   try {
     const response = await fetch(
-      `http://localhost:3000/api/auth/getLectureByTeacher/teacher`,
+      `/api/auth/getLectureByTeacher/teacher`,
       {
         credentials: "include",
       }
@@ -319,7 +157,7 @@ if (leaveForm) {
     };
     console.log(leaveData);
     try {
-      const res = await fetch("http://localhost:3000/api/auth/request", {
+      const res = await fetch("/api/auth/request", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
